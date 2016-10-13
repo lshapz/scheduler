447efd8a139698aef213ea3439399aca2a3ba1db
lshapz <laurashapiro01@gmail.com>
2016-10-13 12:19:13 -0400

    can collect avail and insert into employee_shifts_no_good

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,24 +2,44 @@ class Employee < ActiveRecord::Base
 
   has_many :shifts
   
-  attr_accessor :cannot_work, :name, :id, :shift_hash, :nope
 
   def get_nope
     @nope = gets.chomp
   end 
-
+  
   def self.populate
      DB.execute("insert into employees (name) values ('frank'), ('nicole'), ('ricardo'), ('tony')")
   end 
+
   
-  def cannot_work(*nope)
-    @nope = nope
-    nope.each {|x|
-       if self.shift_hash.flatten.designation == "x" 
-        self.shift_hash.flatten.find_by(designation: "x") = true
-       end}
+  def cannot_work(nope)
+
+    nah = nope.split(", ")
+    me = self.id 
+    # @nope = nope
+    # nope.each {|x|
+    #    if self.shift_hash.flatten.designation == "x" 
+    #     self.shift_hash.flatten.find_by(designation: "x") = true
+    #    end}
+       #undefined method `each' for nil:NilClass
+       #nicole.shift_hash.first.first.designation finds it
+       #nicole.shift_hash.flatten.first.designation finds it 
     #nah.each {|x| insert into employee_shifts_no_good (shift_id, employee_id) VALUES (x.id where x == shift.designation) (self.id)}
+    nah.each {|x|   shift =  Shift.find_by(designation: x).id
+        DB.execute ("insert into employee_shifts_no_good (employee_id, shift_id) VALUES (#{me}, #{shift})")
+
+    }
   end 
+
+  def see_when_you_cannot_work
+    sql = <<-SQL
+      select shifts.designation, employees.name from employee_shifts_no_good 
+      join employees on employee_shifts_no_good.employee_id = employees.id 
+      join shifts on employee_shifts_no_good.shift_id = shifts.id;
+    SQL
+    DB.execute(sql)
+  end
+
 
   def shift_hash
     y = {}
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,14 +2,20 @@ class Shift < ActiveRecord::Base
 
   belongs_to :week
   belongs_to :employee 
-
-  attr_accessor :weekday, :am_or_pm, :week, :employee, :designation
   
-  def initialize(weekday: , am_or_pm: )
-    @weekday = weekday 
-    @am_or_pm = am_or_pm  
-    @week = Self.week
-  end 
+  # def initialize(weekday: , am_or_pm: )
+  #   @weekday = weekday 
+  #   @am_or_pm = am_or_pm  
+  #   @week = Self.week
+  # end 
+
+  # def designation 
+  #   self.designation
+  # end
+
+  # def id
+  #   self.id
+  # end 
 
   def self.populate
     sequel = <<-SQL
@@ -18,11 +24,11 @@ class Shift < ActiveRecord::Base
     DB.execute(sequel)
   end 
   
-  def symbolize
-    day = weekday[0..1]
-    time = am_or_pm[0]
-    ":#{day}#{time}" 
-  end 
+  # def symbolize
+  #   day = weekday[0..1]
+  #   time = am_or_pm[0]
+  #   ":#{day}#{time}" 
+  # end 
   
   # def full?
   #   if self.find_by(id: self.id)
